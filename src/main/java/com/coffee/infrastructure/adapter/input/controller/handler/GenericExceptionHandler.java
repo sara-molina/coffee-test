b627e85aeb669c56db586e23534afcd37373eacf
sara-molina <60813149+sara-molina@users.noreply.github.com>
2025-06-09 20:52:39 +0200

    fix Exceptionhandler

--- src/main/java/com/coffee/infrastructure/adapter/input/controller/handler/GenericExceptionHandler.java
+++ src/main/java/com/coffee/infrastructure/adapter/input/controller/handler/GenericExceptionHandler.java
@@ -17,7 +17,7 @@ public class GenericExceptionHandler {
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorModelApi> handleNotFoundException(Exception ex) {
+  public ResponseEntity<ErrorModelApi> handleGenericException(Exception ex) {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(
             ErrorModelApi.builder()
